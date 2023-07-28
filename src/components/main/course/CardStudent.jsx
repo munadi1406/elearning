492145f0f5288f232bfab4f8eb4ffef2baebc8b1
munadi1406 <munadifathullah123@gmail.com>
2023-07-28 10:33:54 +0800

    fix(cardStudent/main): fix error build ,Universe on cardStudent

--- src/components/main/course/CardStudent.jsx
+++ src/components/main/course/CardStudent.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import Universe from '/assets/Universe.jpg'
+import Universe from '../../../assets/universe.jpg'
 import { FaWhatsapp } from 'react-icons/fa'
 import ScaleEffectMotion from '../../../utils/ScaleEffectMotion'
 
@@ -12,7 +12,7 @@ const CardStudent = ({ name, img, number }) => {
     return (
         <div className='flex justify-between items-center border w-full p-2 text-white rounded-md bg-blue1'>
             <div className='flex justify-start items-center gap-2'>
-                <img src={img ? img : Universe} alt={name} className='w-8 h-8 rounded-full' />
+                <img src={img} alt={name} className='w-8 h-8 rounded-full' />
                 <div className='text-base font-semibold font-sans uppercase'>{name}</div>
             </div>
             <ScaleEffectMotion>
@@ -30,6 +30,8 @@ CardStudent.propTypes = {
     img: PropTypes.string.isRequired,
     number: PropTypes.string.isRequired
 }
-
+CardStudent.defaultProps = {
+    img: Universe
+}
 
 export default CardStudent
