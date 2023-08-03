ce3266c276c4591962b8f60a5047256960b28768
munadi1406 <munadifathullah123@gmail.com>
2023-08-03 14:00:00 +0800

    update(attendace/main): change camera to back camera

--- src/components/main/absensi/check-in/QrCodeCheckin.jsx
+++ src/components/main/absensi/check-in/QrCodeCheckin.jsx
@@ -2,29 +2,29 @@ import { useState } from "react";
 import { QrReader } from 'react-qr-reader'; //sementara saja menunggu versi terbaru
 
 
-export default function QrCodeCheckin() {
-  const [decodedText,setDecodedTex] = useState('result')
-  const [data, setData] = useState('No result');
+export default function QrCodeCheckin() { 
+ const [data, setData] = useState('No result');
+
+  const handleResult = (result,error)=>{
+    if(result){
+      setData(result.text)
+    }else{
+      console.log(error)
+    }
+    
+  }
 
   return (
     <div >
-    <div className="w-full bg-blue1 text-lg text-white text-center ">{toString(decodedText)}</div>
+    <div className="w-full bg-blue1 text-lg text-white text-center ">{data}</div>
 
     <QrReader
         delay={300}
-        onResult={(result, error) => {
-          if (result) {
-            setData(result?.text);
-          }
-
-          if (error) {
-            console.info(error);
-          }
-        }}
+        onResult={handleResult}
         constraints={{facingMode:"environment"}}
         style={{ width: '100%' }}
       />
-      <p>{data}</p>
+      
     </div>
   );
 }
