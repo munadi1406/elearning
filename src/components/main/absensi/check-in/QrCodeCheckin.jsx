e96cd530340b18ca4a777358eaa508756e411381
munadi1406 <munadifathullah123@gmail.com>
2023-08-03 13:36:52 +0800

    update(attendace/main): change wr code scanner fro html5qrcode to qr code reaeder

--- src/components/main/absensi/check-in/QrCodeCheckin.jsx
+++ src/components/main/absensi/check-in/QrCodeCheckin.jsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
-import ScannerPluggin from "../../../../utils/ScannerPluggin";
+// import ScannerPluggin from "../../../../utils/ScannerPluggin";
+import { QrReader } from 'react-qr-reader';
+
 
 export default function QrCodeCheckin() {
   const [decodedText,setDecodedTex] = useState('result')
-  const handleCodeSuccess = ({decodedText}) => {
-    setDecodedTex(decodedText)
-    // console.log(e)
+  // const handleCodeSuccess = ({decodedText}) => {
+  //   setDecodedTex(decodedText)
+  //   // console.log(e)
 
-  };
+  // };
+  const [data, setData] = useState('No result');
+  const handleResult = (e)=>{
+    console.log({e})
+  }
   return (
     <div >
     <div className="w-full bg-blue1 text-lg text-white text-center ">{decodedText}</div>
-      <ScannerPluggin
-        fps={10}
-        qrbox={250}
-        disableFlip={false}
-        qrCodeSuccessCallback={handleCodeSuccess}
-        aspectRatio={1.333334}
+    <QrReader
+        onResult={handleResult}
+        style={{ width: '100%' }}
       />
+      <p>{data}</p>
     </div>
   );
 }
